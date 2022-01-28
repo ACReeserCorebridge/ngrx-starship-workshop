f48511a6b530220b45d89b40333770d3c1a20c41
Justin Fitzpatrick <justin@corebridge.net>
2022-01-28 10:21:37 -0700

    Fixes to get to Zero failures

--- src/app/workshop/computer.service.ts
+++ src/app/workshop/computer.service.ts
@@ -1,8 +1,9 @@
+import { verifyHostBindings } from "@angular/compiler";
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { AppState } from "../app.state";
-import { IComputerDirective } from "../challenge.service";
-import { echo, loadNavData } from "./computer.actions";
+import { IComputerDirective, Verb, Adverb, DirectObject, AdjPhrase, SolarSystemLocation } from "../challenge.service";
+import { echo, loadNavData, setDockingClamp, setEnginePower, setLaserPower, setShieldPower, setTractorBeam, setTractorView, setViewScreenState } from "./computer.actions";
 
 /**
  * computer service to interface between captain's commands and ngrx store
@@ -27,13 +28,73 @@ export class ComputerService{
     public InterpretDirectives(directives: IComputerDirective[]){
         //TODO: decide which actions to dispatch based on the directives passed in!
         directives.forEach(x => this.store.dispatch(
-            //TODO: you don't have to echo all the directives, do what you want!
             echo(
                 {
                     message: this.directiveToMessage(x)
                 }
             )
         ));
+        directives.forEach(directive => 
+            {
+                switch(directive.verb) 
+                {
+                    case 'engage':
+                        var power:number = 1; //slowly
+                        if (directive.adverb == 'fully') power = 10;
+                        if (directive.adverb == 'halfway') power = 5;                    
+                        switch(directive.directObject) {
+                            case 'shields':
+                                this.store.dispatch(setShieldPower({value: power}));
+                                break;
+                            case 'laser':
+                                this.store.dispatch(setLaserPower({value: power}));
+                                break;
+                            case 'engines':
+                                this.store.dispatch(setEnginePower({value: power}));
+                                break;
+                            case 'tractorbeam':
+                                this.store.dispatch(setTractorBeam({value: true}));
+                                break;
+                            case 'docking clamp':
+                                this.store.dispatch(setDockingClamp({value: true}))
+                                break;
+                        }
+                    break;
+                    case 'disengage':
+                        switch(directive.directObject) {
+                            case 'shields':
+                                this.store.dispatch(setShieldPower({value: 0}));
+                                break;
+                            case 'laser':
+                                this.store.dispatch(setLaserPower({value: 0}));
+                                break;
+                            case 'engines':
+                                this.store.dispatch(setEnginePower({value: 0}));
+                                break;
+                            case 'tractorbeam':
+                                this.store.dispatch(setTractorBeam({value: false}));
+                                break;
+                            case 'docking clamp':
+                                this.store.dispatch(setDockingClamp({value: false}))
+                                break;
+                        }
+                    break;
+                    case 'plot':
+                        if (directive.directObject==='course') {
+                            switch (directive.adjectivalPhrase) {
+                                case 'to Luna orbit':                                
+                                    this.store.dispatch(setViewScreenState({value: 'LunaOrbit'}));
+                                    break;
+                                case 'to the asteroid belt':                                
+                                    this.store.dispatch(setViewScreenState({value: 'AsteroidBelt'}));
+                                    break;
+                                case 'to LEO':                                
+                                    this.store.dispatch(setViewScreenState({value: 'LEO'}));
+                                break;
+                            }
+                        }
+                }
+            });
     }
 
     /**
