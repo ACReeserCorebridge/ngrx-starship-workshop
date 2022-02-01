4997d00e3ff7d39dcff80cfac9f7974a07c67761
Clifford <cliffordl@corebridge.net>
2022-02-02 03:27:32 +0800

    NGRX workshop. Fix Failures

--- src/app/workshop/computer.selectors.ts
+++ src/app/workshop/computer.selectors.ts
@@ -5,66 +5,56 @@
  * 
  * this file should be free of any business logic or Math.random() calls!
  */
-import { createSelector } from "@ngrx/store";
-import { selectComputer } from "../app.state";
-import { SolarSystemLocation } from "../challenge.service";
-import { ComputerState } from "./computer.reducer";
-import { ViewscreenState } from "./viewscreen/viewscreen.component";
-
-// https://ngrx.io/guide/store/selectors
-
-export const selectViewscreen = createSelector(
-    selectComputer,
-    (state: ComputerState) => {
-        //TODO: remove all the random state!
-        const locations: SolarSystemLocation[] = ['LEO', 'LunaOrbit', 'AsteroidBelt'];
-        const planets = ['/assets/mars.png', '/assets/SunRed.png', undefined];
-        const satellites = ['/assets/red_asteroid.png', '/assets/yellow_satellite.png', undefined];
-        const view: ViewscreenState = {
-            location: locations[Math.floor(Math.random() * 3)],
-            course: locations[Math.floor(Math.random() * 4)],
-            leftImage: satellites[Math.floor(Math.random() * 3)],
-            centerImage: planets[Math.floor(Math.random() * 3)],
-            rightImage: satellites[Math.floor(Math.random() * 3)],
-            laser: Math.random() > 0.5,
-            tractor: Math.random() > 0.5,
-        };
-        return view;
-    }
-);
-
-export const selectEngine = createSelector(
-    selectComputer,
-    (state: ComputerState) => {
-        //TODO: remove all the random state!
-        return Math.floor(Math.random()*11)
-    }
-);
-
-export const selectLasers = createSelector(
-    selectComputer,
-    (state: ComputerState) => {
-        //TODO: remove all the random state!
-        return Math.floor(Math.random()*11)
-    }
-);
-
-export const selectDockingClamp = createSelector(
-    selectComputer,
-    (state: ComputerState) => {
-        //TODO: remove all the random state!
-        return Math.random() > 0.5
-    }
-);
-
-//TODO: finish up the shield selector!
-// export const selectShields = createSelector(
-//     ??,
-//     (??) => ??
-// );
-
-//TODO: finish up the tractorbeam selector!
-// export const selectTractorbeam = createSelector(
-//     ??,
-//     (??) => ??
-// );
+ import { createSelector } from "@ngrx/store";
+ import { selectComputer } from "../app.state";
+ import { SolarSystemLocation } from "../challenge.service";
+ import { ComputerState } from "./computer.reducer";
+ import { ViewscreenState } from "./viewscreen/viewscreen.component";
+ 
+ // https://ngrx.io/guide/store/selectors
+ 
+ export const selectViewscreen = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         return state.viewScreenState;
+     }
+ );
+ 
+ export const selectEngine = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         //TODO: remove all the random state!
+         return state.expectationState.engine || 0;
+     }
+ );
+ 
+ export const selectLasers = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         //TODO: remove all the random state!
+         return state.expectationState.laser || 0;
+     }
+ );
+ 
+ export const selectDockingClamp = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         //TODO: remove all the random state!
+         return state.expectationState.docking || false;
+     }
+ );
+ 
+ export const selectShields = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         return state.expectationState.shield || 0;
+     }
+ );
+ 
+ export const selectTractorbeam = createSelector(
+     selectComputer,
+     (state: ComputerState) => {
+         return state.expectationState.tractorbeam || false;
+     }
+ );
+ 
--- src/app/workshop/computer.service.ts
+++ src/app/workshop/computer.service.ts
@@ -2,38 +2,45 @@ import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { AppState } from "../app.state";
 import { IComputerDirective } from "../challenge.service";
-import { echo, loadNavData } from "./computer.actions";
+import { plotCourse, dockingClamp, echo, engines, laserPower, loadNavData, shields, tractorbeam } from "./computer.actions";
 
 /**
  * computer service to interface between captain's commands and ngrx store
  * 
  * this service is fully customizable, but all logic should be in the actions/reducers
  */
+ export const MAX_AMT_POWER = 10;
+ export const MIN_AMT_POWER = 1;
+ export const ENGAGE = 'engage';
+export const DISENGAGE = 'disengage';
 @Injectable({
     providedIn: 'root'
 })
-export class ComputerService{
-    constructor(private store: Store<AppState>){}
+export class ComputerService {
+    constructor(private store: Store<AppState>) { }
 
     /**
      * this is called on the captain's very first voice event
      */
-    public Initialize(){
+    public Initialize() {
         this.store.dispatch(loadNavData());
     }
     /**
      * this is called when the captain commands the computer to do one or more things
      */
-    public InterpretDirectives(directives: IComputerDirective[]){
+    public InterpretDirectives(directives: IComputerDirective[]) {
         //TODO: decide which actions to dispatch based on the directives passed in!
-        directives.forEach(x => this.store.dispatch(
-            //TODO: you don't have to echo all the directives, do what you want!
-            echo(
-                {
-                    message: this.directiveToMessage(x)
-                }
-            )
-        ));
+        directives.forEach(x => {
+            switch(x.directObject) {
+                case 'shields' :  this.store.dispatch(shields({ directive: x })); break;
+                case 'engines' :  this.store.dispatch(engines({ directive: x })); break;
+                case 'laser' : this.store.dispatch(laserPower({ directive: x })); break;
+                case 'tractorbeam' : this.store.dispatch(tractorbeam({ directive: x })); break;
+                case 'docking clamp' : this.store.dispatch(dockingClamp({ directive: x })); break;
+                case 'course' : this.store.dispatch(plotCourse({ directive: x })); break;
+            }
+            this.store.dispatch(echo({ message: this.directiveToMessage(x) }));
+        });
     }
 
     /**
@@ -43,7 +50,7 @@ export class ComputerService{
      * @param d 
      * @returns 
      */
-    private directiveToMessage(d: IComputerDirective): string{
+    private directiveToMessage(d: IComputerDirective): string {
         let result = "ACK > ";
         if (d.adverb)
             result += d.adverb.toUpperCase() + " ";
@@ -55,3 +62,25 @@ export class ComputerService{
         return result;
     }
 } 
+
+/** Added Conditions for Computer Directives */
+
+export function processComputerDirectives(directive: IComputerDirective)  {
+    let processDirective: any = undefined;
+    if (directive.adverb) {
+        switch(directive.adverb) {
+            case 'fully':  processDirective = MAX_AMT_POWER; break;
+            case 'halfway': processDirective = MAX_AMT_POWER / 2; break;
+            default: processDirective = MIN_AMT_POWER;  /** Default Slowly */
+        }
+    }
+    if (directive.adjectivalPhrase) {
+        switch(directive.adjectivalPhrase) {
+            case 'to the asteroid belt' : processDirective = 'AsteroidBelt';  break;
+            case 'to Luna orbit' : processDirective = 'LunaOrbit';  break;
+            case 'to LEO' : processDirective = 'LEO';  break;
+            default : processDirective = undefined;
+        }
+    }
+    return processDirective;
+ }
