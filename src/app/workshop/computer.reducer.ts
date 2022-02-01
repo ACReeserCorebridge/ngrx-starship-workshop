24dbae922ded0459b11af401645ae4a1c9c7c834
Gregory Schroeder <gschroeder@corebridge.net>
2022-02-01 12:59:07 -0600

    Goal #1 Completed:  Zero Failures

--- src/app/workshop/computer.reducer.ts
+++ src/app/workshop/computer.reducer.ts
@@ -3,9 +3,11 @@
  * 
  * all main computer logic should go in this file
  */
+import { act } from "@ngrx/effects";
 import { createReducer, on } from "@ngrx/store";
+import { SolarSystemLocation } from "../challenge.service";
 import { NavigationData } from "../nav-db.service";
-import { echo, loadNavDataSuccess } from "./computer.actions";
+import { adjustDockingClamp, adjustEngines, adjustLasers, adjustShields, adjustTractorBeam, echo, loadNavDataError, loadNavDataSuccess, plotCourse } from "./computer.actions";
 
 /**
  * This is the "slice" that you need to fill out!
@@ -24,13 +26,27 @@ export interface ComputerState{
      * 
      * feel free to change or remove this
      */
-    echoMessages: string[]
-    //TODO: add a lot more state!
+    navigationData: NavigationData[];
+    echoMessages: string[];
+    tractorbeamEngaged: boolean;
+    shields: number;
+    docked: boolean;
+    engines: number;
+    lasers: number;
+    location: SolarSystemLocation;
+    course: SolarSystemLocation|undefined;
 }
 
 export const InitialComputerState: ComputerState = {
-    echoMessages: []
-    //TODO: add additional initial state!
+    navigationData: [],
+    echoMessages: [],
+    tractorbeamEngaged: false,
+    shields: 0,
+    docked: false,
+    engines: 0,
+    lasers: 0,
+    location: 'LEO',
+    course: undefined
 }
 
 export const computerReducer = createReducer<ComputerState>(
@@ -44,11 +60,121 @@ export const computerReducer = createReducer<ComputerState>(
             ]
         };
     }),
-    //TODO: add an on() listener for loadNavDataSuccess that puts NavigationData[] in the state!
-    //TODO: use the NavigationData[] to set viewscreen state depending on location and/or course!
-    //TODO: add a lot more reducer action logic!
-    // https://ngrx.io/guide/store/reducers
-    // there should be a lot of logic in here!
-);
 
- 
+    on(loadNavDataSuccess, (state, action) => {
+        return {
+            ...state,
+            navigationData: action.navs
+        }
+    }),
+    
+    on(adjustEngines, (state, action) => {
+        let course = state.course;
+        let location = state.location;
+
+        if (course) {
+            location = course;
+            if (action.level == 0) 
+                course = undefined;
+        }
+
+        return {
+            ...state,
+            engines : action.level,
+            lasers: 0,
+            location: location,
+            course: course
+        }
+    }),
+
+    on(adjustLasers, (state, action) => {
+        let navData = [...state.navigationData];
+
+        if (action.level > 0.5) {
+            const index = navData.findIndex(x => x.location == state.location);
+            let leftImage = navData[index].leftImage;
+            if (leftImage?.includes("asteroid"))
+                leftImage = undefined;
+
+            let centerImage = navData[index].centerImage;
+            if (centerImage?.includes("asteroid"))
+                centerImage = undefined;
+
+            let rightImage = navData[index].rightImage;
+            if (rightImage?.includes("asteroid"))
+                rightImage = undefined;
+
+            navData[index] = 
+            {
+                location: state.location,
+                leftImage: leftImage,
+                centerImage: centerImage,
+                rightImage: rightImage
+            }
+        }
+
+        return {
+            ...state,
+            navigationData: navData,
+            lasers : action.level
+        }
+    }),
+
+    on(plotCourse, (state, action) => {
+        return {
+            ...state,
+            course : action.location,
+            lasers: 0
+        }
+    }),
+
+    on(adjustDockingClamp, (state, action) => {
+        return {
+            ...state,
+            docked : action.docking,
+            lasers: 0
+        }
+    }),
+
+    on(adjustShields, (state, action) => {
+        return {
+            ...state,
+            shields: action.level,
+            lasers: 0
+        };
+    }),
+
+    on(adjustTractorBeam, (state, action) => {
+        let navData = [...state.navigationData];
+
+        if (!action.engaged) {
+            const index = navData.findIndex(x => x.location == state.location);
+            let leftImage = navData[index].leftImage;
+            if (leftImage?.includes("satellite"))
+                leftImage = undefined;
+
+            let centerImage = navData[index].centerImage;
+            if (centerImage?.includes("satellite"))
+                centerImage = undefined;
+
+            let rightImage = navData[index].rightImage;
+            if (rightImage?.includes("satellite"))
+                rightImage = undefined;
+
+            navData[index] = 
+            {
+                location: state.location,
+                leftImage: leftImage,
+                centerImage: centerImage,
+                rightImage: rightImage
+            }
+        }
+
+        return {
+            ...state,
+            navigationData: navData,
+            tractorbeamEngaged: action.engaged,
+            lasers: 0
+        };
+    }),
+);
