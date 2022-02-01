dcc9a21eeef10cbeca388ec7267a1b482a31fbd5
jdreeser <jdreeser@users.noreply.github.com>
2022-01-31 21:06:02 -0600

    finished hiding state

--- src/app/workshop/computer.reducer.ts
+++ src/app/workshop/computer.reducer.ts
@@ -5,8 +5,26 @@
  */
 import { createReducer, on } from "@ngrx/store";
 import { NavigationData } from "../nav-db.service";
-import { echo, loadNavDataSuccess } from "./computer.actions";
+import { echo, loadNavData, loadNavDataError, loadNavDataSuccess } from "./computer.actions";
 import { SolarSystemLocation } from "../challenge.service";
+
+export class HiddenState {
+    nav: NavigationData[] = [];
+    /**
+     * What? You think that me hiding the state inside of echoMessages is cheesy?
+     * I'll go one step further and condense the non-navigation data down to an
+     * encoded string. Here I'm not just reducing state keys, but I'm reducing
+     * the state down to an approximate low byte representation. It could be even
+     * lower if I made this into a single number that was composed by doing a
+     * bitwise OR on all inputs. That's a BIT too much work for me, though.
+     *
+     *                     REFERENCE GUIDE
+     * shield engine laser docking tractor cargo course location
+     * 0      1      2     3       4       5     6      7
+     * 0x     0x     0x    0b      0b      0b    0x     0x
+     */
+    data: string = "00000000";
+}
 
 /**
  * This is the "slice" that you need to fill out!
@@ -24,34 +42,32 @@ export interface ComputerState{
      * they are not required or useful, they are just example of state properties
      *
      * feel free to change or remove this
+     *
+     * BWAHAHAH!!!! Not useful!? Oh I beg to differ!
      */
     echoMessages: string[]
-    //TODO: add a bit more state!
-    state: string;
 }
 
 export const InitialComputerState: ComputerState = {
-    echoMessages: [],
-    //TODO: add additional initial state!
-    // shield engine laser docking tractor cargo course location
-    // 0      1      2     3       4       5     6      7
-    // 0x     0x     0x    0b      0b      0b    0x     0x
-    state: "00000000"
+    echoMessages: [JSON.stringify(new HiddenState())]
 }
 
 export const computerReducer = createReducer<ComputerState>(
     InitialComputerState,
     on(echo, (state, action) => {
         return {
-            ...state,
-            echoMessages: [
-                action.message,
-                ...state.echoMessages
-            ]
+            ...state
         };
     }),
-    //TODO: add an on() listener for loadNavDataSuccess that puts NavigationData[] in the state!
-    //TODO: use the NavigationData[] to set viewscreen state depending on location and/or course!
+    on(loadNavDataSuccess, (state, action) => {
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        secret.nav = action.navs;
+        return {
+            echoMessages: [
+                JSON.stringify(secret)
+            ]
+        }
+    })
     //TODO: add a lot more reducer action logic!
     // https://ngrx.io/guide/store/reducers
     // there should be a lot of logic in here!
--- src/app/workshop/computer.selectors.ts
+++ src/app/workshop/computer.selectors.ts
@@ -8,7 +8,8 @@
 import { createSelector } from "@ngrx/store";
 import { selectComputer } from "../app.state";
 import { SolarSystemLocation } from "../challenge.service";
-import { ComputerState } from "./computer.reducer";
+import { NavigationData } from "../nav-db.service";
+import { ComputerState, HiddenState } from "./computer.reducer";
 import { ViewscreenState } from "./viewscreen/viewscreen.component";
 
 // https://ngrx.io/guide/store/selectors
@@ -16,54 +17,64 @@ import { ViewscreenState } from "./viewscreen/viewscreen.component";
 export const selectViewscreen = createSelector(
     selectComputer,
     (state: ComputerState) => {
-        //TODO: remove all the random state!
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
         const locations: SolarSystemLocation[] = ['LEO', 'LunaOrbit', 'AsteroidBelt'];
-        const planets = ['/assets/planet.png', '/assets/luna.png', '/assets/asteroid.png'];
-        const satellites = ['/assets/red_asteroid.png', '/assets/yellow_satellite.png', undefined];
+        let curr: NavigationData = {
+            location: 'LEO',
+            leftImage: undefined,
+            centerImage: undefined,
+            rightImage: undefined
+        } as NavigationData;
+        if (secret.nav) {
+            let index = Number(`0x${secret.data[7]}`);
+            curr = secret.nav.find(x => x.location == locations[index])!;
+        }
         const view: ViewscreenState = {
-            location: locations[Number(`0x${state.state[7]}`)],
-            course: locations[Number(`0x${state.state[6]}`)],
-            leftImage: satellites[Math.floor(Math.random() * 3)],
-            centerImage: planets[Math.floor(Math.random() * 3)],
-            rightImage: satellites[Math.floor(Math.random() * 3)],
-            laser: Number(`0x${state.state[2]}`) >= 5,
-            tractor: state.state[4] === "1",
+            ...curr,
+            course: locations[Number(`0x${secret.data[6]}`)],
+            laser: Number(`0x${secret.data[2]}`) >= 5,
+            tractor: secret.data[4] === "1",
         };
         return view;
     }
 );
 
 export const selectShields = createSelector(
-  selectComputer,
-  (state: ComputerState) => {
-      return Number(`0x${state.state[0]}`);
-  }
+    selectComputer,
+    (state: ComputerState) => {
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        return Number(`0x${secret.data[0]}`);
+    }
 );
 
 export const selectEngine = createSelector(
     selectComputer,
     (state: ComputerState) => {
-        return Number(`0x${state.state[1]}`);
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        return Number(`0x${secret.data[1]}`);
     }
 );
 
 export const selectLasers = createSelector(
     selectComputer,
     (state: ComputerState) => {
-        return Number(`0x${state.state[2]}`);
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        return Number(`0x${secret.data[2]}`);
     }
 );
 
 export const selectDockingClamp = createSelector(
     selectComputer,
     (state: ComputerState) => {
-        return state.state[3] === "1";
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        return secret.data[3] === "1";
     }
 );
 
 export const selectTractorbeam = createSelector(
-  selectComputer,
-  (state: ComputerState) => {
-        return state.state[4] === "1";
-  }
+    selectComputer,
+    (state: ComputerState) => {
+        let secret: HiddenState = JSON.parse(state.echoMessages[0]);
+        return secret.data[4] === "1";
+    }
 );
